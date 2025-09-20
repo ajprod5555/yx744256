ec238b53076afd100fdfd460e43516316c2e52b7
ajprod5555 <ajprod5555@gmail.com>
2025-09-19 20:52:57 -0400

    Add: .clasp.json, .claspignore, .github/workflows/main.yml, Code.js, README.md, appsscript.json | Edit .gitignore; little refactor

---
+++ Code.js
@@ -0,0 +1,14 @@
+
+function UPDATED__() {
+  // .. ..2
+}
+
+__deps = () => {
+  SpreadsheetApp.getActiveRangeList(); // ..
+  DriveApp.getFolderById(); // ..
+  DriveApp.createFolder();
+  MailApp.sendEmail(); // ..
+};
+__$$dd = (...params) => ContentService.createTextOutput(JSON.stringify(params, null, 2)); // ..
+doGet  = (e) => __$$dd('.. ..2 !!!!! 2025-09-19 [PLS] AGAIN ..' + 555); // ..
+doPost = (e) => __$$dd(4567); // ..
---
+++ README.md
@@ -0,0 +1,4 @@
+
+# README
+
+![main](https://github.com/ajprod5555/yx439556/actions/workflows/main.yml/badge.svg?branch=yxatt885)
